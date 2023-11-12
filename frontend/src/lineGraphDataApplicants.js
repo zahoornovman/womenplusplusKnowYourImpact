15919c65600800e0e8007173d81571b8daa5749b
alesordo <ale.sordo@hotmail.it>
2023-11-12 15:31:57 +0100

    Fixed

--- frontend/src/lineGraphDataApplicants.js
+++ frontend/src/lineGraphDataApplicants.js
@@ -1,16 +1,15 @@
 const lineGraphDataApplicants = ({data, data1, data2}) => {
-    const length = Object.keys(data).length - 1;
-    const length1 = Object.keys(data1).length - 1;
-    const length2 = Object.keys(data2).length - 1;
-  
-    const dataPie = [
-      { year: 2018, number: 600 },
-      { year: 2019, number: 800 },
-      { year: 2023, number: length2 }
-    ];
-  
-    return dataPie;
-  };
-  
-  export default lineGraphDataApplicants;
-  
+  const length = Object.keys(data).length - 1;
+  const length1 = Object.keys(data1).length - 1;
+  const length2 = Object.keys(data2).length - 1;
+
+  const dataPie = [
+    { year: 2018, number: 600 },
+    { year: 2019, number: 800 },
+    { year: 2023, number: length2 }
+  ];
+
+  return dataPie;
+};
+
+export default lineGraphDataApplicants;
--- frontend/src/routes/Graphs.jsx
+++ frontend/src/routes/Graphs.jsx
@@ -1,4 +1,4 @@
-import BarChart from "../components/BarChart";
+// import Histogram from "../components/Histogram";
 import PieChart from "../components/PieChart";
 import { useState, useEffect } from "react";
 import * as d3 from "d3";
@@ -9,49 +9,28 @@ import LineGraph from "../components/LineGraph";
 import ScatterPlot from "../components/ScatterPlot";
 import lineGraphDataApplicants from "../lineGraphDataApplicants";
 
-import "./graphs.scss";
-import barChartData from "../educationData";
-
 export default function Graphs() {
+  const [data, setData] = useState(null);
+
   const dataLineChart = [10, 20, 30, 40, 50, 40, 30, 20, 10];
 
   const [dataCSV, setDataCsv] = useState(null);
   const [genderData, setGenderDataPie] = useState({});
   const [backgroundData, setBackgroundDataPie] = useState({});
   const [satisfactionData, setSatisfactionDataPie] = useState({});
-  const [educationData, setEducationData] = useState({});
-
-  const data = [
-    { label: "A", value: 20 },
-    { label: "B", value: 50 },
-    { label: "C", value: 30 },
-    // Add more data points as needed
-  ];
   const [applicantsData, setAppliedDataLine] = useState([{}]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Assuming the CSV file is named 'data.csv' and is in the 'src' folder
-        const response = await d3.csv(
-          "../src/assets/data_participants_2023_2.csv"
-        );
-        const response1 = await d3.csv(
-          "../src/assets/data_participants_2019_2.csv"
-        );
-        const response2 = await d3.csv(
-          "../src/assets/data_participants_2018_2.csv"
-        );
+        const response = await d3.csv("../src/assets/data_participants_2023_2.csv");
+        const response1 = await d3.csv("../src/assets/data_participants_2019_2.csv");
+        const response2 = await d3.csv("../src/assets/data_participants_2018_2.csv");
         setGenderDataPie(pieChartDataGender(response));
         setBackgroundDataPie(pieChartDataBackground(response));
         setSatisfactionDataPie(pieChartDataSatisfaction(response));
-        setAppliedDataLine(
-          lineGraphDataApplicants({
-            data: response,
-            data1: response1,
-            data2: response2,
-          })
-        );
+        setAppliedDataLine(lineGraphDataApplicants({data: response, data1: response1, data2: response2}));
       } catch (error) {
         console.error("Error fetching or parsing data", error);
       }
@@ -61,15 +40,13 @@ export default function Graphs() {
   }, []);
 
   return (
-    <div className="graphs-main">
-      <div className="bar-chart">
-        <BarChart data={educationData} />
-      </div>
+    <>
+      {/* <Histogram /> */}
       <PieChart data={genderData} />
       <PieChart data={backgroundData} />
       <PieChart data={satisfactionData} />
-      <LineGraph data={applicantsData} />
+      <LineGraph data={applicantsData}/>
       <ScatterPlot />
-    </div>
+    </>
   );
 }

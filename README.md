2774cb179c5c2cc9b6c443d633bad566918b7fff
alesordo <ale.sordo@hotmail.it>
2023-11-12 16:37:29 +0100

    Added README and random participant generation

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# [Hack n Lead](https://womenplusplus.ch/hacknlead)
+# TrackYourImpact
 
-===
+## What is it?
 
-## Steps
+It's a new, open way to measure the impact of non-profits. This tool aims to provide users with the right amount of knowledge to invest their time and money in organisations that produce actual good.
 
-1. Use this repository as a template (or Fork it)
-2. Add your team members as contributors
-3. Put your presentation in the `docs/` folder
-4. This repository **must** be open source (and licensed) in order to submit
-5. Add the tag `hack-n-lead` to the repo description
+The indicators used for the measurement are innovative and based on several studies about this topic. Moreover, we provide non-profit companies with a consulting service in order to collect data effectively for their events and with stakeholders.
 
+The philosophy behind TrackYourImpact is to be open and collaborative, so we ask developers and companies to send us their data and contributions. In the future we will also provide the datasets used for analysis to everybody that needs it.
 
+## Why are we different?
+
+There are existing non-profit trackers, but they are just products for internal evaluation. Since we believe that transparency is the key we offer a new vision of that, not limited to companies' vision only.
+
+## How ready is your product?
+
+We have a Proof of Concept available as source code. We generated the data, as it's very difficult to retrieve it from open sources (that's why we want to do it in the future :smile:).
+
+In the Proof of Concept you'll find the basic interface and the dashboard viualization. It all works with csv files, by uploading them in our project we retrieve them and use d3.js to plot the data.
+
+## What technologies you used?
+
+We developed the website frontend using React and d3.js. A backend would be much needed to store and retrieve the data correctly and also to provide users with login functionalities.
---
+++ dataGeneration/random_participants.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 18:37:54 2023
+
+@author: Ana Lugo
+"""
+
+import pandas as pd
+import numpy as np
+from faker import Faker
+from random import randint 
+
+# create some fake data
+fake = Faker()
+
+# function to create a dataframe with fake values for participants
+def make_participants(num):
+    
+    # lists to randomly assign to participants
+    gender_list = ['Male', 'Female', 'Non-binary']
+    education_list = ['PhD','Undergraduate','Masters degree', 'Prefer not to say']
+    accepted_list = ['Yes','No']
+    attended_list = ['Yes','No']
+    backgroundit_list = ['Yes', 'No']
+    satisfaction_list = ['very dissatisfied', 'satisfied', 'neutral', 'dissatisfied']
+    employment_list = ['Senior Associate','Manager','Associate','Junior','Senior Manager']
+    careerinterests_list = ['solutions design','pm','azure','python','.net','javascript','sql']
+    newskill_list = ['UI/UX', 'Software Development','Pitch','Data Scientist','full stack developer']
+    
+    fake_participants = [{'ID': randint(1, 1000),
+        'name':fake.name(), 
+        'Applied': 'Yes',
+        'gender': np.random.choice(gender_list),
+        'age':np.random.randint(25,60),
+        'background in IT': np.random.choice(backgroundit_list),
+        'education': np.random.choice(education_list),
+        'accepted': np.random.choice(accepted_list),
+        'attended': np.random.choice(attended_list),
+        'number of Hackathons': np.random.randint(0,10),
+        'employment': np.random.choice(employment_list),
+        'satisfaction': np.random.choice(satisfaction_list),
+        'career interests': np.random.choice(careerinterests_list),
+        'country': fake.country(),
+        'new skill': np.random.choice(newskill_list)
+        
+    }]
+        
+    return fake_participants
+
+participants_df = pd.DataFrame(make_participants(num=1))
+
+i=1
+while i < 1000:
+    participants_df = participants_df.append(pd.DataFrame(make_participants(num=1)))
+    i += 1
+participants_df.to_csv('data_participants_2023_2).csv')
